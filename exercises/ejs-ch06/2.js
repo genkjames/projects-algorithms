4c007dda2c01c0917a623a1ec6f5748e081c2296
Genaya James <genayakjames@gmail.com>
2018-09-03 10:59:29 -0400

    completed exercise 2 from ch06

---
+++ exercises/ejs-ch06/2.js
@@ -0,0 +1,70 @@
+// The standard JavaScript environment provides another data structure called Set. Like an instance of Map, a set holds a collection of values. Unlike Map, it does not associate other values with those—it just tracks which values are part of the set. A value can be part of a set only once—adding it again doesn’t have any effect.
+
+// Write a class called Group (since Set is already taken). Like Set, it has add, delete, and has methods. Its constructor creates an empty group, add adds a value to the group (but only if it isn’t already a member), delete removes its argument from the group (if it was a member), and has returns a Boolean value indicating whether its argument is a member of the group.
+
+// Use the === operator, or something equivalent such as indexOf, to determine whether two values are the same.
+
+// Give the class a static from method that takes an iterable object as argument and creates a group that contains all the values produced by iterating over it.
+
+class Group {
+  constructor() {
+    this.values = []
+  }
+
+  add(value) {
+    const index = this.values.indexOf(value);
+    if (index === -1) this.values.push(value);
+  }
+
+  delete(value) {
+    this.values = this.values.filter(val => val !== value);
+  }
+
+  has(value) {
+    return this.values.includes(value);
+  }
+
+  static from(values) {
+    let group = new Group;
+    for (let el of values) {
+      group.add(el);
+    }
+    return group;
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+console.log(group.has(30));
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+
+// Other solution
+// class Group {
+//   constructor() {
+//     this.members = [];
+//   }
+
+//   add(value) {
+//     if (!this.has(value)) {
+//       this.members.push(value);
+//     }
+//   }
+
+//   delete(value) {
+//     this.members = this.members.filter(v => v !== value);
+//   }
+
+//   has(value) {
+//     return this.members.includes(value);
+//   }
+
+//   static from(collection) {
+//     let group = new Group;
+//     for (let value of collection) {
+//       group.add(value);
+//     }
+//     return group;
+//   }
+// }
